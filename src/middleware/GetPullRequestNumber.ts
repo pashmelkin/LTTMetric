29cbd1eb50e75355fae97c0403b97549cc5e373e
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-09-04 17:30:52 +1000

    Add branch match

--- src/middleware/GetPullRequestNumber.ts
+++ src/middleware/GetPullRequestNumber.ts
@@ -1,14 +1,14 @@
 import {GetPullRequestsAsync }  from "../utils/GetPullRequests" ;
 import {PullRequest} from "../models/PullRequest";
 
-export async function GetPullRequestNumber(owner: string, repo: string, branch: string) {
+export async function GetPullRequestNumber(owner: string, repo: string, cardId: string) {
     let myPulls: PullRequest[] = [];
     let error: string = "";
 
     let prs = await GetPullRequestsAsync(owner, repo, "closed");
     prs.data.forEach(pr => {
 
-        if (pr.base.ref === "master" && pr.head.ref === branch) {
+        if (pr.base.ref === "master" && pr.head.ref.toLowerCase().startsWith(cardId.toLowerCase() )) {
             myPulls.push(new PullRequest(pr.number, pr.head.ref, pr.merge_commit_sha));
         }
     });
