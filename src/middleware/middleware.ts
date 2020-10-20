7c08608d6f1b9b744c075c2b791f349158bce773
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-10-20 14:29:19 +1100

    attempt

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,6 +1,12 @@
-import {IMiddleware} from '../models/interfaces/IMiddleware';
 import {GetPullRequests} from './GetPullRequests';
 import {GetCommitDate} from "./GetCommitDate";
+
+interface IMiddleware {
+    GetPullRequests: (owner: string, repo: string, branchPattern: string)
+        => PromiseLike<{ myPulls: any; error: any; }> | { myPulls: any; error: any; };
+    GetCommitDate: (owner: string, repo: string, pull_number: number) => any
+
+};
 
 
 const middleware: IMiddleware = {
